c1331231a477b46cdfe38b660e1f7941a0895952
Panjala Sruthi <143275010+sruthipanjala@users.noreply.github.com>
2024-02-25 23:06:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let x = 2;
+console.log(x);
+
+const PI = 3.141592653589793;
+console.log(PI);
+
+console.log("hello world");
+
+//data types
+number1 = 10;
+console.log(typeof(number1));
+
+char = "careerX";
+console.log(typeof("char"));
+
+number2 = 3.2425;
+console.log(typeof(number2));
+
+// Booleans
+let number3 = true;
+let number4 = false;
+console.log(typeof(number3));
+
+let y = 16 + "Volvo";
+console.log(typeof(y));
